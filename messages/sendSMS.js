5c6d85f235059f040ab927183b55c6c08c693ea4
kelche <kelcheone@gmail.com>
2024-03-15 14:36:10 +0300

    config: remove .env from the folders and made sendSMS use the new structure

--- messages/sendSMS.js
+++ messages/sendSMS.js
@@ -2,22 +2,23 @@ import AfricasTalking from "africastalking";
 import dotenv from "dotenv";
 dotenv.config();
 const africastalking = AfricasTalking({
-    apiKey: '',
-    username: ''
-})
-
+  apiKey: process.env.API_KEY,
+  username: process.env.USERNAME,
+});
 
 const sendSMS = (PhoneNumber, rate) => {
-    const sms = africastalking.SMS;
-    const options = {
-        to: PhoneNumber,
-        message: `The current BTC/USD exchange rate is ${rate}. Thank you for using our service`,
-    }
-    sms.send(options).then(response => {
-        console.log(response);
-    }).catch(error => {
-        console.log(error);
+  const sms = africastalking.SMS;
+  const options = {
+    to: PhoneNumber,
+    message: `The current BTC/USD exchange rate is ${rate}. Thank you for using our service`,
+  };
+  sms
+    .send(options)
+    .then((response) => {
+      console.log(response);
+    })
+    .catch((error) => {
+      console.log(error);
     });
-}
+};
 export default sendSMS;
-
