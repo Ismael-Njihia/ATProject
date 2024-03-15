368b7988cca52da741d8f3e5db0c1dae60916275
Ismael njihia <engishmaelnjihia@gmail.com>
2024-03-15 13:37:42 +0300

    small changes

--- messages/sendSMS.js
+++ messages/sendSMS.js
@@ -2,8 +2,8 @@ import AfricasTalking from "africastalking";
 import dotenv from "dotenv";
 dotenv.config();
 const africastalking = AfricasTalking({
-    apiKey: '1334221841e0824ffa9d9ac6532805a6cbb81666763dce35027d48bbcfeff8c5',
-    username: 'sms001'
+    apiKey: '',
+    username: ''
 })
 
 
